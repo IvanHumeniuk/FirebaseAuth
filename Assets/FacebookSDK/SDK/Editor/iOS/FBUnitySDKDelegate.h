1a4f469fe213077b7135de59b9e816aafbe5d15d
IvanHumeniuk <ivan.humeniuk.acc@gmail.com>
2021-12-01 13:50:11 +0200

    [ADD] FAcebook SDK

---
+++ Assets/FacebookSDK/SDK/Editor/iOS/FBUnitySDKDelegate.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f034c87755166d435f4ae1e1c31810bbbd766181db6bd47cbf1b9d21bb097c9d
+size 2405
---
+++ Assets/FacebookSDK/SDK/Editor/iOS/FBUnityUtility.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64c8d50e047ac07b18a1585599ebc299b382f61d021f4dcc9ce9d83d7e424e95
+size 2102
