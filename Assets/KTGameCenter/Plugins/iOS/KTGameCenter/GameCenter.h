b940f54215bb01cf36669fdfce8a532a07943b57
Evgeniy Smelianskyi <evgeniy.smelianskyi@gmail.com>
2021-12-02 14:08:11 +0200

    [ADD] 5 BUCKS GameCenter plugin

---
+++ Assets/KTGameCenter/Plugins/iOS/KTGameCenter/GameCenter.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba6fd398205da82198bd436d988ba2106ae161128de8aeb40b860222cdd5184d
+size 1403
---
+++ Assets/KTGameCenter/Plugins/iOS/KTGameCenter/GameCenterManager.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1f055479659ea8cd683275b97c24e0f20c92bb28e05cc9726811a3234a6121f
+size 3942
